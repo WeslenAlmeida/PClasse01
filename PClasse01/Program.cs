0364e3e5c04895666886682ce969b2921ea215e0
WeslenAlmeida <weslen1202@gmail.com>
2022-08-23 20:36:18 -0300

    Adicionar arquivos de projeto.

---
+++ PClasse01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PClasse01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
