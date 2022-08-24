ac9793c4e03900737688a187f9df5bc8a82e34ce
WeslenAlmeida <weslen1202@gmail.com>
2022-08-23 21:44:16 -0300

    Inserindo classe Pessoa

---
+++ PClasse01/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PClasse01
+{
+    internal class Pessoa
+    {
+        String nome { get; set; }
+        String dataNasc { get; set; }
+        String cpf { get; set; }
+        String telefone { get; set; }
+        
+        public Pessoa(String nome, String dataNasc, String cpf, String telefone)
+        {
+            this.nome = nome;
+            this.dataNasc = dataNasc;   
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome} \nData Nasc: {dataNasc} \nCPF: {cpf} \nTelefone: {telefone}";
+        }
+
+    }
+}
--- PClasse01/Program.cs
+++ PClasse01/Program.cs
@@ -6,7 +6,43 @@ namespace PClasse01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa[] pessoas = new Pessoa[3];
+
+            for(int i = 0; i < pessoas.Length; i++)
+            {
+                string[] dados = Cadastro(i);
+                pessoas[i] = new Pessoa(dados[0], dados[1], dados[2], dados[3]);
+            }
+
+            Console.Clear();
+
+            for(int i = 0; i < pessoas.Length; i++)
+            {
+                Console.WriteLine($"\n{i+1}° CADASTRO");
+                Console.WriteLine(pessoas[i].ToString());
+            } 
+
+
+        }
+        static string[] Cadastro(int cont)
+        {
+            string[] dados = new string[4];
+
+            Console.WriteLine($"{cont+1}° CADASTRO");
+            Console.Write($"Digite seu nome: ");
+            dados[0] = Console.ReadLine();
+
+            Console.Write($"Digite sua data de nascimento: ");
+            dados[1] = Console.ReadLine();
+
+            Console.Write($"Digite seu CPF: ");
+            dados[2] = Console.ReadLine();
+
+            Console.Write("Digite seu telefone: ");
+            dados[3] = Console.ReadLine();
+            Console.WriteLine();
+
+            return dados;
         }
     }
 }
